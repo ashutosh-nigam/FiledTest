a98ea633324053faa754a4bbb947f91a497d8faf
Ashutosh Nigam <ashutosh.nigam@live.com>
2021-02-28 23:30:12 +0530

    Created DB Context and Payment EntityModel

---
+++ FiledTest.Data/DataModels/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FiledTest.Data.DataModels
+{
+    public class Payment
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [StringLength(maximumLength: 16, MinimumLength = 16)]
+        public string CreditCardNumber { get; set; }
+        [StringLength(16, MinimumLength = 8)]
+        public string CardHolder { get; set; }
+
+        // Can not save as per data security guidelines
+        //public string ExpirationDate { get; set; }
+        //public string SecurityCode { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+        public PaymentStatus Status { get; set; }
+
+        public enum PaymentStatus
+        {
+            Success,
+            Invalid,
+            Error
+        }
+    }
+}
---
+++ FiledTest.Data/FiledTestDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FiledTest.Data
+{
+    public class FiledTestDataContext : DbContext
+    {
+        public FiledTestDataContext(DbContextOptions<FiledTestDataContext> dbContextOptions): base(dbContextOptions)
+        {
+           
+        }
+        public DbSet<DataModels.Payment> Payments { get; set; }
+    }
+}
