ab00d60254208af3feae77205c1836027228816f
Ashutosh Nigam <ashutosh.nigam@live.com>
2021-03-11 23:26:33 +0530

    Added Test Projects

---
+++ FiledTest.Services.UnitTests/DBUtitlity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiledTest.Services.UnitTests
+{
+    public class DBUtitlity
+    {
+        FiledTest.Data.FiledTestDataContext filedTestDataContext;
+        public DBUtitlity()
+        {
+            filedTestDataContext = new Data.FiledTestDataContext();
+        }
+    }
+}
